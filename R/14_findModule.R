39de711754b2b8663c3eb3afd37ff9bfec6c40d2
Exceret <15364051195@163.com>
2025-12-08 08:59:41 +0800

    update

--- R/14_findModule.R
+++ R/14_findModule.R
@@ -9,26 +9,26 @@
 #' @export
 #'
 findModule <- function(H, tred = 2, do.dip = FALSE) {
-    K <- nrow(H)
-    meanH <- rowMeans(H)
-    sdH <- SigBridgeRUtils::rowSds3(H)
+  K <- nrow(H)
+  meanH <- SigBridgeRUtils::rowMeans3(H)
+  sdH <- SigBridgeRUtils::rowSds3(H)
 
-    if (!do.dip) {
-        threshold_matrix <- H - meanH > tred * sdH
-        module <- apply(threshold_matrix, 1, which, simplify = FALSE)
-        return(module)
+  if (!do.dip) {
+    threshold_matrix <- H - meanH > tred * sdH
+    module <- apply(threshold_matrix, 1, which, simplify = FALSE)
+    return(module)
+  }
+
+  module <- vector("list", K)
+  for (i in seq_len(K)) {
+    x <- H[i, ]
+    if (diptest::dip.test(x)$p.value < 0.05) {
+      modes <- multimode::locmodes(x, mod0 = 2)
+      module[[i]] <- which(x > modes$locations[2])
+    } else {
+      module[[i]] <- which(x - meanH[i] > tred * sdH[i])
     }
+  }
 
-    module <- vector("list", K)
-    for (i in seq_len(K)) {
-        x <- H[i, ]
-        if (diptest::dip.test(x)$p.value < 0.05) {
-            modes <- multimode::locmodes(x, mod0 = 2)
-            module[[i]] <- which(x > modes$locations[2])
-        } else {
-            module[[i]] <- which(x - meanH[i] > tred * sdH[i])
-        }
-    }
-
-    module
+  module
 }
